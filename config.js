4894370a18142cc45d3152b8369d3bcc90265aea
AndreRibeiro2019 <56743827+AndreRibeiro2019@users.noreply.github.com>
2021-05-26 10:14:54 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,68 @@
+const products = [
+  {
+    name: 'Calça Feminina Adidas',
+    price: 312.33,
+    description: 'Descrição',
+    category: 'Feminina',
+    image: '../../images/calca-f/image1.jpg'
+  },
+  {
+    name: 'Calça Feminina Adidas 2',
+    price: 320.33,
+    description: 'Descrição',
+    category: 'Feminina',
+    image: '../../images/calca-f/image2.jpg'
+  },
+  {
+    name: 'Calça Feminina Adidas 2',
+    price: 120.33,
+    description: 'Descrição',
+    category: 'Feminina',
+    image: '../../images/calca-f/image3.jpg'
+  },
+  {
+    name: 'Calça Feminina Adidas 2',
+    price: 90.33,
+    description: 'Descrição',
+    category: 'Feminina',
+    image: '../../images/calca-f/image4.jpg'
+  },
+  {
+    name: 'Calça Feminina Adidas 2',
+    price: 88.33,
+    description: 'Descrição',
+    category: 'Feminina',
+    image: '../../images/calca-f/image5.png'
+  },
+];
+
+const menuItens = [
+  {
+    name: 'Home',
+    href: '',
+  },
+  {
+    name: 'Acessórios',
+    href: '',
+  },
+  {
+    name: 'Calçados',
+    href: '',
+  },
+  {
+    name: 'Feminino',
+    href: '',
+  },
+  {
+    name: 'Masculino',
+    href: '',
+  },
+  {
+    name: 'Infantil',
+    href: '',
+  },
+  {
+    name: 'Inverno',
+    href: '',
+  },
+];
